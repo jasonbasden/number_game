48451f10f1171168ff9ffa62d4cc3d5efd502271
Jason Basden <JasonBasden@ou.edu>
2020-03-27 23:00:26 -0500

    Cleaned up the code.

--- number_game.py
+++ number_game.py
@@ -4,14 +4,12 @@ ranNum = random.randint(0,10)
 
 userNum = int(input("What is your number?: "))
 
-#ranNum != userNum
-
 while ranNum != userNum:
     if(ranNum > userNum):
         print("Your number is lower, guess again!")
         userNum = int(input("What is your number?: "))
     else:
-        print("Your number is hight, guess again!")
+        print("Your number is higher, guess again!")
         userNum = int(input("What is your number?: "))
 if(ranNum == userNum):
     print("You guessed the number!" + str(ranNum))
