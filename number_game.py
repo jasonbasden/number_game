863cae0ab4817cf58653eef9bedf8f84ad588ce5
Jason Basden <JasonBasden@ou.edu>
2020-03-27 22:29:15 -0500

    continuing while loop

--- number_game.py
+++ number_game.py
@@ -4,12 +4,19 @@ ranNum = random.randint(0,10)
 
 userNum = int(input("What is your number?: "))
 
-while ranNum != userNum:
+ranNum != userNum
+
+while True:
     if(ranNum > userNum):
         print("Your number is lower, guess again!")
+        userNum = int(input("What is your number?: "))
+        break
     else:
         print("Your number is hight, guess again!")
+        userNum = int(input("What is your number?: "))
+        break
+    if(ranNum == userNum):
+        print("Guessed the number!" + ranNum)
+        break
 
-if(ranNum == userNum):
-    print("Guessed the number!" + ranNum)
-else:
+
